9117824c8892417bbbc36134294e945ab3e12d45
Martin Etmajer <m.etmajer@gmail.com>
2016-07-27 15:56:08 +0200

    Minor code beautification.

--- build/loadgen/build/scripts/run-process.sh
+++ build/loadgen/build/scripts/run-process.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 ET_FRONTEND_URL=${ET_FRONTEND_URL:-'http://localhost:8080'}
 
-# Wait for the server to start serving connections.
-echo "Waiting for the easyTravel Customer Frontend on ${ET_FRONTEND_URL}"
-wait-for-cmd.sh "nc -z `echo ${ET_FRONTEND_URL} | sed 's/http:\/\///' | sed 's/:/ /'`" 360
-
 if [ -n "${ET_BACKEND_URL}" ]; then
   echo "Waiting for the easyTravel Business Backend on ${ET_BACKEND_URL}"
   wait-for-cmd.sh "nc -z `echo ${ET_BACKEND_URL} | sed 's/http:\/\///' | sed 's/:/ /'`" 360
   run-problem-patterns.sh &
 fi
 
+echo "Waiting for the easyTravel Customer Frontend on ${ET_FRONTEND_URL}"
+wait-for-cmd.sh "nc -z `echo ${ET_FRONTEND_URL} | sed 's/http:\/\///' | sed 's/:/ /'`" 360
 java -jar uemload.jar --autorun true --server ${ET_FRONTEND_URL}

80256dc51de1c62b312a88e24d4268499359d91b
Martin Etmajer <m.etmajer@gmail.com>
2016-07-27 10:56:37 +0200

    Make the NGINX reverse proxy also proxy the backend on port 8080.

--- build/loadgen/build/scripts/run-process.sh
+++ build/loadgen/build/scripts/run-process.sh
@@ -5,5 +5,10 @@ ET_FRONTEND_URL=${ET_FRONTEND_URL:-'http://localhost:8080'}
 echo "Waiting for the easyTravel Customer Frontend on ${ET_FRONTEND_URL}"
 wait-for-cmd.sh "nc -z `echo ${ET_FRONTEND_URL} | sed 's/http:\/\///' | sed 's/:/ /'`" 360
 
-run-problem-patterns.sh &
+if [ -n "${ET_BACKEND_URL}" ]; then
+  echo "Waiting for the easyTravel Business Backend on ${ET_BACKEND_URL}"
+  wait-for-cmd.sh "nc -z `echo ${ET_BACKEND_URL} | sed 's/http:\/\///' | sed 's/:/ /'`" 360
+  run-problem-patterns.sh &
+fi
+
 java -jar uemload.jar --autorun true --server ${ET_FRONTEND_URL}
--- build/nginx/build/scripts/run-process.sh
+++ build/nginx/build/scripts/run-process.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-ET_FRONTEND_LOCATION=${ET_FRONTEND_LOCATION:-'localhost:8080'}
+if [ -n "${ET_FRONTEND_LOCATION}" ]; then
+  echo "Waiting for the easyTravel Customer Frontend on ${ET_FRONTEND_LOCATION}"
+  wait-for-cmd.sh "nc -z `echo ${ET_FRONTEND_LOCATION} | sed 's/:/ /'`" 360
 
-echo "Waiting for the easyTravel Customer Frontend on ${ET_FRONTEND_LOCATION}"
-wait-for-cmd.sh "nc -z `echo ${ET_FRONTEND_LOCATION} | sed 's/:/ /'`" 360
+  # Replace the upstream server location template variable with the actual value.
+  sed -i -e "s,\$ET_FRONTEND_LOCATION,${ET_FRONTEND_LOCATION}," /etc/nginx/nginx.conf
+fi
 
-# Replace the upstream server location template variable with the actual value.
-sed -i -e "s,\$ET_FRONTEND_LOCATION,${ET_FRONTEND_LOCATION}," /etc/nginx/nginx.conf
+if [ -n "${ET_BACKEND_LOCATION}" ]; then
+  echo "Waiting for the easyTravel Business Backend on ${ET_BACKEND_LOCATION}"
+  wait-for-cmd.sh "nc -z `echo ${ET_BACKEND_LOCATION} | sed 's/:/ /'`" 360
+
+  # Replace the upstream server location template variable with the actual value.
+  sed -i -e "s,\$ET_BACKEND_LOCATION,${ET_BACKEND_LOCATION}," /etc/nginx/nginx.conf
+fi
+
 nginx -g 'daemon off;'

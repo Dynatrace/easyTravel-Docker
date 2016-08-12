bdd1a5d2b872b38614188a44d928327db3942761
Martin Etmajer <m.etmajer@gmail.com>
2016-08-12 10:03:05 +0200

    Add option to delay the activation of problem patterns via ET_PROBLEMS_DELAY.

--- build/loadgen/build/scripts/run-process.sh
+++ build/loadgen/build/scripts/run-process.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ET_FRONTEND_URL=${ET_FRONTEND_URL:-'http://localhost:8080'}
+ET_PROBLEMS_DELAY=${ET_PROBLEMS_DELAY:-0}
 
 if [ -n "${ET_BACKEND_URL}" ]; then
   echo "Waiting for the easyTravel Business Backend on ${ET_BACKEND_URL}"
   wait-for-cmd.sh "nc -z `echo ${ET_BACKEND_URL} | sed 's/http:\/\///' | sed 's/:/ /'`" 360
-  run-problem-patterns.sh &
+  (sleep ${ET_PROBLEMS_DELAY} && run-problem-patterns.sh) &
 fi
 
 echo "Waiting for the easyTravel Customer Frontend on ${ET_FRONTEND_URL}"

114a15cb9e3ca2a1c0af57081a2874e83b09ecd7
Maciej Browarczyk <maciej.browarczyk@dynatrace.com>
2021-08-18 13:55:58 +0200

    APM-316861 fix env variables for mongo
    GitOrigin-RevId: 9dff97638270f2ecdc8a9ed75cc56bafac862779

--- images/mongodb-content-creator/build/scripts/run-process.sh
+++ images/mongodb-content-creator/build/scripts/run-process.sh
@@ -5,4 +5,4 @@ ET_DATABASE_LOCATION=${ET_DATABASE_LOCATION:-'localhost:27017'}
 echo "Waiting for the easyTravel database on ${ET_DATABASE_LOCATION}"
 wait-for-cmd.sh "nc -z `echo ${ET_DATABASE_LOCATION} | sed 's/:/ /'`" 360
 
-java ${JAVA_OPTS} -cp com.dynatrace.easytravel.launcher.jar -Dconfig.mongoDbInstances=${ET_DATABASE_LOCATION} -Dconfig.mongoDbUser=${ET_MONGODB_USER} -Dconfig.mongoDbAuthDatabase=${ET_MONGODB_AUTH_DB} -Dconfig.mongoDbPassword=${ET_MONGODB_PASSWORD} com.dynatrace.easytravel.launcher.war.DatabaseContentCreator mongod	b
+java ${JAVA_OPTS} -cp com.dynatrace.easytravel.launcher.jar -Dconfig.mongoDbInstances=${ET_DATABASE_LOCATION} -Dconfig.mongoDbUser=${ET_DATABASE_USER} -Dconfig.mongoDbAuthDatabase=${ET_MONGO_AUTH_DB} -Dconfig.mongoDbPassword=${ET_DATABASE_PASSWORD} com.dynatrace.easytravel.launcher.war.DatabaseContentCreator mongodb

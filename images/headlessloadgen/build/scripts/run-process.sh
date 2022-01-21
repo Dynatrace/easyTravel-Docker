6ddc7610f6a959d5505e3095e12dd48e1e087fe9
Rafal Psciuk <Rafal.Psciuk@dynatrace.com>
2022-01-21 17:15:39 +0100

    Pull request #14: APM-344906 Add parameter for specyfing number of chromes
    Merge in DEM/easytravel-dockerbuilder from APM-344906_add_max_chrome_parameter to master
    
    Squashed commit of the following:
    
    commit c6384ba42a292e2c143b13479207b31118b712b0
    Author: rafal.psciuk <rafal.psciuk@dynatrace.com>
    Date:   Fri Jan 21 16:51:24 2022 +0100
    
        Add parameter for specyfing number of chromes
    
    GitOrigin-RevId: 3c0d9ee633167a01afb54c9f29122b67de1a854d

--- images/headlessloadgen/build/scripts/run-process.sh
+++ images/headlessloadgen/build/scripts/run-process.sh
@@ -4,6 +4,8 @@ ET_PROBLEMS_DELAY=${ET_PROBLEMS_DELAY:-0}
 ET_VISIT_NUMBER=${ET_VISIT_NUMBER:-3}
 EXTRA_OPTS="--installationMode ${ET_APM_SERVER_DEFAULT:-APM}"
 SCENARIO="${SCENARIO_NAME:-Headless Angular Scenario}"
+ET_PROBLEMS_DELAY=${ET_PROBLEMS_DELAY:-0}
+MAX_CHROME_DRIVERS=${MAX_CHROME_DRIVERS:-1}
 
 if [ -n "${ET_BACKEND_URL}" ]; then
   echo "Waiting for the easyTravel Business Backend on ${ET_BACKEND_URL}"
@@ -13,4 +15,4 @@ fi
 
 echo "Waiting for the easyTravel Customer Frontend on ${ET_ANGULAR_FRONTEND_URL}"
 wait-for-cmd.sh "nc -z `echo ${ET_ANGULAR_FRONTEND_URL} | sed 's/http:\/\///' | sed 's/:/ /'`" 360
-java ${JAVA_OPTS} -Dconfig.chromeDriverOpts=no-sandbox,disable-dev-shm-usage -Dconfig.maximumChromeDrivers=1 -Dconfig.maximumChromeDriversMobile=1 -jar uemload.jar --autorun true --const ${ET_VISIT_NUMBER} --server ${ET_ANGULAR_FRONTEND_URL} ${EXTRA_OPTS} --scenario "${SCENARIO}"
+java ${JAVA_OPTS} -Dconfig.chromeDriverOpts=no-sandbox,disable-dev-shm-usage -Dconfig.maximumChromeDrivers=${MAX_CHROME_DRIVERS} -Dconfig.maximumChromeDriversMobile=${MAX_CHROME_DRIVERS} -jar uemload.jar --autorun true --const ${ET_VISIT_NUMBER} --server ${ET_ANGULAR_FRONTEND_URL} ${EXTRA_OPTS} --scenario "${SCENARIO}"
